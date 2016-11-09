0e4fb91a4d3f67916c414ae5e453dbbd860eb674
JuanMa Garrido <JuanMa.Garrido@gmail.com>
2016-11-09 10:31:41 +0100

    post working and adding tasks

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const bodyparser = require('body-parser')
 const app = express()
 const PORT = 3000;
 
@@ -15,10 +16,21 @@ let tasks = [
 
 app.set('view engine', 'pug')
 app.use( express.static('public') )
+app.use( bodyparser.urlencoded({ extended: false }) )
 
 app.get('/', (req,res) => {
 	const title = "TODO Tasks"
 	res.render('index', { title, tasks } )
+})
+
+// curl --request POST 'http://localhost:3000/task' --data 'desc=My new task'
+app.post('/task', (req,res) => {
+
+	var newTask = req.body;
+	newTask.done = false;
+	tasks.push(newTask);
+
+	res.sendStatus(200)
 })
 
 app.listen(PORT, () => console.log(`Listening on port ${PORT}...`) )
