cad9ceca6aa4a77937a998d847b0417f7088c7ac
JuanMa Garrido <JuanMa.Garrido@gmail.com>
2016-11-14 15:22:23 +0100

    render w/ data got from mongodb

--- app.js
+++ app.js
@@ -1,76 +1,98 @@
 const express = require('express')
 const bodyparser = require('body-parser')
+
+const MongoClient = require('mongodb').MongoClient;
 const app = express()
 const PORT = 3000;
 
-let tasks = [
-	{
-		id: 0,
-		desc: 'Flight tickets to Canada',
-		done: false
-	},
-	{
-		id: 1,
-		desc: 'Buy gold',
-		done: false
-	}
-]
-let counter = 2;
+const urlDB = 'mongodb://localhost:27017/test'
+
+// let tasks = [
+// 	{
+// 		id: 0,
+// 		desc: 'Flight tickets to Canada',
+// 		done: false
+// 	},
+// 	{
+// 		id: 1,
+// 		desc: 'Buy gold',
+// 		done: false
+// 	}
+// ]
+// let counter = 2;
 
 app.set('view engine', 'pug')
 app.use( express.static('public') )
 app.use( bodyparser.urlencoded({ extended: false }) )
 
-app.get('/', (req,res) => {
-	const title = "TODO Tasks"
-	const pending = tasks.filter( task => task.done === false );
-	res.render('index', { title, pending } )
-})
+MongoClient.connect( urlDB )
+	.then( db => {
 
-app.get('/completed', (req,res) => {
-	const title = "COMPLETED Tasks"
-	const completed = tasks.filter( task => task.done === true );
-	res.render('completed', { title, completed } )
-})
+		console.log("connected to DB...")
 
-app.get('/tasks', (req,res) => {
-	res.json(tasks)
-})
+		app.get('/', (req,res) => {
 
-// curl --request POST 'http://localhost:3000/task' --data 'desc=My new task'
-app.post('/task', (req,res) => {
-	var newTask = req.body;
-	newTask.done = false;
-	newTask.id = counter++;
-	tasks.push(newTask);
-	res.redirect('/');
-})
+			const title = "TODO Tasks"
 
-// curl --request DELETE 'http://localhost:3000/task/2'
-app.delete('/task/:id', (req,res) => {
-	const id = +req.params.id;
-	tasks = tasks.filter( task => task.id != id );
-	console.log(tasks)
-	res.sendStatus(200)
-})
+			db.collection('tasks')
+				.find( { done: false })
+				.toArray()
+				.then( pending => {
+					res.render('index', { title, pending } )
+				})
 
-// curl -X PUT http://localhost:3000/task/2 --data done=true
-app.put('/task/:id', (req,res) => {
-	const id = +req.params.id;
+		})
 
-	const isDone = (req.body.done === "true") ? true : false;
-	console.log (isDone)
-	console.log (id)
+			//const pending = tasks.filter( task => task.done === false );
 
-	tasks = tasks.map( task => {
-		if ( task.id === id ) {
-			task.done = isDone
-		}
-		return task;
-	});
+	})
+	.catch( err => console.log(err) )
 
-	res.sendStatus(200)
-})
+
+// app.get('/completed', (req,res) => {
+// 	const title = "COMPLETED Tasks"
+// 	const completed = tasks.filter( task => task.done === true );
+// 	res.render('completed', { title, completed } )
+// })
+
+// app.get('/tasks', (req,res) => {
+// 	res.json(tasks)
+// })
+
+// // curl --request POST 'http://localhost:3000/task' --data 'desc=My new task'
+// app.post('/task', (req,res) => {
+// 	var newTask = req.body;
+// 	newTask.done = false;
+// 	newTask.id = counter++;
+// 	tasks.push(newTask);
+// 	res.redirect('/');
+// })
+
+// // curl --request DELETE 'http://localhost:3000/task/2'
+// app.delete('/task/:id', (req,res) => {
+// 	const id = +req.params.id;
+// 	tasks = tasks.filter( task => task.id != id );
+// 	console.log(tasks)
+// 	res.sendStatus(200)
+// })
+
+// // curl -X PUT http://localhost:3000/task/2 --data done=true
+// app.put('/task/:id', (req,res) => {
+// 	const id = +req.params.id;
+
+// 	const isDone = (req.body.done === "true") ? true : false;
+// 	console.log (isDone)
+// 	console.log (id)
+
+// 	tasks = tasks.map( task => {
+// 		if ( task.id === id ) {
+// 			task.done = isDone
+// 		}
+// 		return task;
+// 	});
+
+// 	res.sendStatus(200)
+// })
 
 
 app.listen(PORT, () => console.log(`Listening on port ${PORT}...`) )
